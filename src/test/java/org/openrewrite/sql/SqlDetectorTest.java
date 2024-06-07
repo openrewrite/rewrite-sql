ae16798e85b35e3e5d43774b36dee1598323b25b
Tim te Beek <timtebeek@gmail.com>
2024-06-07 09:27:26 +0000

    refactor: OpenRewrite best practices
    Use this link to re-run the recipe: https://app.moderne.io/recipes/builder/BftezDRNh?organizationId=T3BlblJld3JpdGU%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/test/java/org/openrewrite/sql/SqlDetectorTest.java
+++ src/test/java/org/openrewrite/sql/SqlDetectorTest.java
@@ -15,10 +15,10 @@
  */
 package org.openrewrite.sql;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class SqlDetectorTest {
 
