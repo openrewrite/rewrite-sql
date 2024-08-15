51f25f5e0f77709d9cfe6912bc8b7585182adacb
Jonathan Schnéider <jkschneider@gmail.com>
2024-08-15 00:37:46 -0400

    Migrate to JSpecify from OpenRewrite JSR-305 meta-annotations (#18)
    Use this link to re-run the recipe: https://app.moderne.io/builder/gW3xaSLWV?organizationId=T3BlblJld3JpdGU%3D
    
    Co-authored-by: Moderne <team@moderne.io>

--- src/main/java/org/openrewrite/sql/FindSql.java
+++ src/main/java/org/openrewrite/sql/FindSql.java
@@ -15,8 +15,8 @@
  */
 package org.openrewrite.sql;
 
+import org.jspecify.annotations.Nullable;
 import org.openrewrite.*;
-import org.openrewrite.internal.lang.Nullable;
 import org.openrewrite.java.JavaIsoVisitor;
 import org.openrewrite.java.tree.*;
 import org.openrewrite.marker.GitProvenance;
